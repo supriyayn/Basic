7e8111c63a91d36d0ed73c8454024421bb5d3b3d
supriyayn <101716883+supriyayn@users.noreply.github.com>
2022-03-18 12:05:42 +0530

    myproj

---
+++ src/basic1/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package basic1;
+
+/**
+ * @author supri
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String []args) {
+		
+		
+		
+		System.out.println("Hello world");
+	}
+}
